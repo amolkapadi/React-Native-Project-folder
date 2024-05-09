ae7b7faec14c7e4e7958df93faa0e381e8b63a79
Amol kapadi <amolakapadi@gmail.com>
2024-05-09 18:02:58 +0530

    first commit

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,27 +1,69 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../Screen/Home/Home';
 import Profile from '../Screen/Profile/Profile';
 import Messanger from '../Screen/Messanger/Messanger';
 import Settings from '../Screen/Settings/Settings';
-
+import { HomeIcon, SettingsIcon, MessageIcon, ProfileIcon} from '../theme/Images';
 const Tab = createBottomTabNavigator();
-
+const CustomTabIcon = ({ source, focused }) => {
+  return (
+    <Image
+      source={source}
+      style={{ 
+        width: 24, 
+        height: 24,
+        tintColor: focused ? 'red' : 'black' // Apply red tint color if tab is active, otherwise black
+      }} 
+    />
+  );
+};
 export default function Navigation() {
   return (
     <Tab.Navigator
     screenOptions={{
         tabBarActiveTintColor: 'red',
         tabBarInactiveTintColor: 'black',
-        tabBarStyle: { paddingBottom: 15, height: 50 },
-        contentStyle: { marginBottom: 10 },
+        tabBarStyle: { paddingBottom: 10, height: 60 },
         headerShown:false  // true
     }}>
-         <Tab.Screen name='Home' component={Home} options={{ tabBarIcon: () => null  }}/>
-         <Tab.Screen name='Profile' component={Profile} options={{ tabBarIcon: () => null  }}/>
-         <Tab.Screen name='Messanger' component={Messanger} options={{ tabBarIcon: () => null  }}/>
-         <Tab.Screen name='Settings' component={Settings} options={{ tabBarIcon: () => null  }}/>
+        <Tab.Screen 
+        name='Home' 
+        component={Home} 
+        options={{ 
+          tabBarIcon: ({ focused }) => (
+            <CustomTabIcon source={HomeIcon} focused={focused} />
+          )
+        }} 
+      />
+      <Tab.Screen 
+        name='Profile' 
+        component={Profile} 
+        options={{ 
+          tabBarIcon: ({ focused }) => (
+            <CustomTabIcon source={ProfileIcon} focused={focused} />
+          )
+        }} 
+      />
+      <Tab.Screen 
+        name='Messanger' 
+        component={Messanger} 
+        options={{ 
+          tabBarIcon: ({ focused }) => (
+            <CustomTabIcon source={MessageIcon} focused={focused} />
+          )
+        }} 
+      />
+      <Tab.Screen 
+        name='Settings' 
+        component={Settings} 
+        options={{ 
+          tabBarIcon: ({ focused }) => (
+            <CustomTabIcon source={SettingsIcon} focused={focused} />
+          )
+        }} 
+      />
     </Tab.Navigator>
   )
 }
--- theme/Images.js
+++ theme/Images.js
@@ -1,4 +1,7 @@
-const Logo = require("../assets/step1.png");
+const HomeIcon = require("../assets/home.png");
+const ProfileIcon = require("../assets/profile.png");
+const MessageIcon = require("../assets/message.png");
+const SettingsIcon = require("../assets/settings.png");
 
 
-export {Logo}
+export {HomeIcon, ProfileIcon, MessageIcon, SettingsIcon}
